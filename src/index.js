17964b26229cd43c8e9d2301bee4cd9429c715d9
Jorge Roldán <icabina@live.com>
2022-04-09 14:00:26 -0500

    m

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import Pages from "./pages";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  // uri: "http://localhost:4000",
+  uri: "https://server-catstronauts-icabina.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
